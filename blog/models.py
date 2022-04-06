ea2301744ced986a2fbd25525db9c5d3d7755f61
matt <matthieu.l.dubray@gmail.com>
2022-04-06 09:42:58 -0700

    adding in models for blog posts and custom user
    I chose to do a custom user to give me more flexibility as well as keep it as similar to nodejs and flask projects that will be made like it

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	image = models.ImageField(upload_to='images', null=True)
+	body = models.TextField()
+	date = models.DateField(auto_now=True)
+	slug = models.SlugField(max_length=100)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', null=True, default=None)
