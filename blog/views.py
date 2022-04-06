1532541dd9e973f6f921cb6a19ad1c5b09720719
matt <matthieu.l.dubray@gmail.com>
2022-04-06 13:20:47 -0700

    basic views, serializers, url register to allow each model to be queried for all entries

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Post
+from .serializers import PostSerializer
+
 
 # Create your views here.
+class PostsViewSet(viewsets.ModelViewSet):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
