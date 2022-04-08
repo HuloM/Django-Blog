4b8a60876692bd91b7352534ae61d0671b21f7e6
matt <matthieu.l.dubray@gmail.com>
2022-04-07 22:15:06 -0700

    password hashing implemented, user cannot login due to encoding bug, user creation now looks for if user with email exists

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,5 @@ router = routers.DefaultRouter()
 router.register('user', views.UserViewSet)
 
 urlpatterns = [
-    path('/', include(router.urls))
+    path('', include(router.urls))
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
+import traceback
+
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import action, parser_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+import bcrypt
 
 from .models import User
 from .serializers import UserSerializer
@@ -14,13 +17,36 @@ class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
+	@action(detail=False, methods=['POST'])
+	@parser_classes([JSONParser])
+	def login(self, request):
+		try:
+			email = request.data['email']
+			password = request.data['password']
+			user = get_object_or_404(self.queryset, email=email)
+			print('test')
+			# TODO figure out TypeError: Unicode-objects must be encoded before checking on user.password
+			if bcrypt.checkpw(password.encode('utf-8'), user.password):
+				response = {'message': 'user was logged in', 'user': user.as_json()}
+				resStatus = status.HTTP_200_OK
+			else:
+				response = {'message': 'Password incorrect'}
+				resStatus = status.HTTP_401_UNAUTHORIZED
+		except:
+			traceback.print_exc()
+			response = {'message': 'login has failed'}
+			resStatus = status.HTTP_422_UNPROCESSABLE_ENTITY
+		return Response(response, status=resStatus)
+
 	@parser_classes(JSONParser)
 	def create(self, request):
 		password = request.data['password']
 		confirmPassword = request.data['confirmPassword']
 
-		# TODO check if user with email already exists
 		email = request.data['email']
+		user = User.objects.get(email=email)
+		if user is not None:
+			return Response({'message': 'User with that email already exists'}, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 		username = request.data['username']
 		first_name = request.data['first_name']
@@ -32,13 +58,12 @@ class UserViewSet(viewsets.ModelViewSet):
 				'last_name': last_name,
 				'email': email
 			}, 'message': 'User failed to create'}, status.HTTP_422_UNPROCESSABLE_ENTITY)
-		# TODO hash password
 		user = User(
 			username=username,
 			first_name=first_name,
 			last_name=last_name,
 			email=email,
-			password=''
+			password=bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt())
 		)
 		user.save()
 		return Response({'user': user.as_json(), 'message': 'User created'}, status.HTTP_200_OK)
@@ -51,8 +76,7 @@ class UserViewSet(viewsets.ModelViewSet):
 		confirmPassword = request.data['confirmPassword']
 		if password != confirmPassword:
 			return Response({'message': 'User failed to update'}, status.HTTP_422_UNPROCESSABLE_ENTITY)
-		# TODO hash password
-		user.password = ''
+		user.password = bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt())
 		user.save()
 		return Response({'user': user.as_json(), 'message': 'user updated'}, status.HTTP_200_OK)
 
