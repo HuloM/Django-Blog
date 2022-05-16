ebace19e4afe60e3efe962e818a0fe14a8c6eed9
matt <matthieu.l.dubray@gmail.com>
2022-05-16 12:26:25 -0700

    adding new host

--- DjangoBlogProject/settings.py
+++ DjangoBlogProject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoblog-env.eba-ggivgiwm.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['djangoblog-env.eba-6k4y2hdn.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
